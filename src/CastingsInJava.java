8ce37f8f2db2e63dc1e56967b0ee4885d893f61a
Anubhav Raj <anubhav.raj.srivastav27@gmail.com>
2024-05-28 18:52:57 +0530

    CastingsInJava

---
+++ src/CastingsInJava.java
@@ -0,0 +1,14 @@
+public class CastingsInJava {
+    public static void main(String[] args) {
+        //Casting in java is a process of converting one data type into another data type.
+        double price = 100.50;
+        double finalprice = price + 18; //Implicit casting - converting int to double
+        System.out.println("Final Price: " + finalprice);
+
+        //Types of casting - Widening (Implicit) and Narrowing (Explicit)
+        //Widening (Implicit) Casting - When you convert a smaller data type to a larger data type
+
+        //Narrowing (Explicit) Casting - When you convert a larger data type to a smaller data type
+
+    }
+}
